636b1d8d8ab8cdfeebbfde7f2bf4b06d8c9a8c4e
Vonschell <vonschell02@gmail.com>
2025-01-13 18:17:01 +0900

    Add am/pm functionality and hour adjustment to showCurrentTime function

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ var lunchtime = 12;
 var naptime = lunchtime + 2;
 var partytime;
 var evening = 18;
+
+
+// Getting it to show the current time on the page
+var showCurrentTime = function()
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+ 
+    var currentTime = new Date();
+ 
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+ 
+    // Set hours
+	  if (hours >= noon)
+	  {
+		  meridian = "PM";
+	  }
+
+	  if (hours > noon)
+	  {
+		  hours = hours - 12;
+	  }
+} 
